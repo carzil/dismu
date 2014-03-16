2245730ace13bc36e4b06a0f475d2402c38feee9
Alexander Andreev <carzil@yandex.ru>
2014-03-17 00:55:28 +0400

    track storage optimized

--- client/src/main/java/com/dismu/music/player/PCTrackStorage.java
+++ client/src/main/java/com/dismu/music/player/PCTrackStorage.java
@@ -7,14 +7,14 @@ import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
+
 import com.dismu.utils.Utils;
 import com.dismu.logging.Loggers;
 
 public class PCTrackStorage implements TrackStorage {
-    private HashMap<Track, File> tracks = new HashMap<Track, File>();
-    private HashSet<Long> trackHashes = new HashSet<Long>();
+    private HashMap<Track, File> tracks = new HashMap<>();
+    private HashMap<Long, Track> trackHashes = new HashMap<>();
     private File trackIndex;
     private int maxTrackID = -1;
 
@@ -37,26 +37,24 @@ public class PCTrackStorage implements TrackStorage {
             Track track = Track.readFromStream(index);
             maxTrackID = Math.max(maxTrackID, track.getID());
             String trackFileName = index.readUTF();
+            long fileHash = index.readLong();
             File trackFile = new File(getTrackFolder(), trackFileName);
-            try {
-                this.trackHashes.add(Utils.getAdler32FileHash(trackFile));
-            } catch (IOException e) {
-                Loggers.playerLogger.error("hash computing failed", e);
-                continue;
-            }
-            Loggers.playerLogger.info("read track from index, id={}, filename='{}'", track.getID(), trackFileName);
-            this.tracks.put(track, trackFile);
+            trackHashes.put(fileHash, track);
+            Loggers.playerLogger.info("read track from index, id={}, hash={}, filename='{}'", track.getID(), fileHash, trackFileName);
+            tracks.put(track, trackFile);
         }
+        Loggers.playerLogger.info("successfully read track index");
     }
 
     private synchronized void saveIndex() throws IOException {
         DataOutputStream index = new DataOutputStream(new FileOutputStream(trackIndex));
-        index.writeInt(this.tracks.size());
+        index.writeInt(tracks.size());
         for (Map.Entry<Track, File> entry : tracks.entrySet()) {
             Track track = entry.getKey();
             String trackName = entry.getValue().getName();
             track.writeToStream(index);
             index.writeUTF(trackName);
+            index.writeLong(Utils.getAdler32FileHash(entry.getValue()));
             Loggers.playerLogger.info("track id={}, name='{}' registered in index", track.getID(), trackName);
         }
         index.flush();
@@ -78,21 +76,27 @@ public class PCTrackStorage implements TrackStorage {
         return tracks.keySet().toArray(new Track[0]);
     }
 
+    /**
+     * Adds track to track index and copies file to local storage
+     * @param trackFile track file to add
+     * @return Track object added to storage
+     */
     public synchronized Track saveTrack(File trackFile) {
         Loggers.playerLogger.info("got track '{}' for saving", trackFile.getAbsolutePath());
         try {
             Track track = Track.fromMp3File(trackFile);
             long fileHash = Utils.getAdler32FileHash(trackFile);
             Loggers.playerLogger.debug("track hash = {}", fileHash);
-            if (trackHashes.contains(fileHash)) {
+            if (trackHashes.containsKey(fileHash)) {
                 Loggers.playerLogger.info("track already registered in index");
-                return null;
+                return trackHashes.get(fileHash);
             } else {
                 maxTrackID++;
                 track.setID(maxTrackID);
                 File finalTrackFile = new File(getTrackFolder(), track.getPrettifiedFileName());
                 Loggers.playerLogger.info("final track name = '{}'", finalTrackFile.getAbsolutePath());
                 tracks.put(track, finalTrackFile);
+                trackHashes.put(fileHash, track);
                 Utils.copyFile(trackFile, finalTrackFile);
                 Loggers.playerLogger.info("track registered in index");
                 saveIndex();
@@ -105,7 +109,15 @@ public class PCTrackStorage implements TrackStorage {
     }
 
     public synchronized void removeTrack(Track track) {
-        tracks.remove(track);
+        try {
+            File trackFile = tracks.get(track);
+            long hash = Utils.getAdler32FileHash(trackFile);
+            trackHashes.remove(hash);
+            tracks.remove(track);
+            Loggers.playerLogger.info("removed track id={}, hash={}, filename='{}'", track.getID(), hash, trackFile.getName());
+        } catch (IOException e) {
+            Loggers.playerLogger.error("exception occurred while removing track", e);
+        }
     }
 
     public synchronized File getTrackFile(Track track) {
--- client/src/main/java/com/dismu/music/player/Player.java
+++ client/src/main/java/com/dismu/music/player/Player.java
@@ -37,20 +37,22 @@ public class Player {
     }
 
     private void runInternal() throws Exception {
-        Track[] tracks = this.trackStorage.getTracks();
-        this.playerBackend.setTrack(tracks[0]);
-        this.playerBackend.play();
-        this.playerBackend.seek(0.5);
-        this.playerBackend.play();
-        Loggers.playerLogger.debug("yep");
-        while (this.playerBackend.isPlaying()) {}
+        Track[] tracks = trackStorage.getTracks();
+//        this.playerBackend.setTrack(tracks[0]);
+//        this.playerBackend.play();
+//        this.playerBackend.seek(0.5);
+//        this.playerBackend.play();
+//        Loggers.playerLogger.debug("yep");
+//        while (this.playerBackend.isPlaying()) {}
 //        Playlist playlist = new Playlist();
 //        playlist.setCycled(true);
 //        playlist.addTrack(tracks[0]);
 //        playlist.setName("#1");
 //        this.playlistStorage.addPlaylist(playlist);
 //        trackStorage.saveTrack(new File("rise_against-prayer_of_the_refugee.mp3"));
-        trackStorage.saveTrack(new File("ra-potr.mp3"));
+        Track track = trackStorage.saveTrack(new File("ra-potr.mp3"));
+        trackStorage.removeTrack(track);
+//        Loggers.playerLogger.info("{} - {}", track.getTrackArtist(), track.getTrackName());
     }
 
 }

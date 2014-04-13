54b14b1c3f5d62b575dc57ca25ab429f2ae659ee
Victor Rudnev <viktorrudnev@gmail.com>
2014-04-13 22:44:27 +0400

    Album Art Downloader.

--- dismu-android/src/main/java/com/dismu/ui/android/HelloAndroidActivity.java
+++ dismu-android/src/main/java/com/dismu/ui/android/HelloAndroidActivity.java
@@ -17,8 +17,7 @@ import com.dismu.music.player.Track;
 import com.dismu.music.storages.TrackStorage;
 import com.dismu.music.storages.events.TrackStorageEvent;
 import com.dismu.p2p.App;
-import com.dismu.p2p.apiclient.API;
-import com.dismu.p2p.client.Client;
+import com.dismu.ui.android.albumart.AlbumArtDownloader;
 import com.dismu.utils.events.Event;
 import com.dismu.utils.events.EventListener;
 import de.mindpipe.android.logging.log4j.LogConfigurator;
@@ -63,53 +62,12 @@ public class HelloAndroidActivity extends Activity {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                App.start("b", localIP, 1337);
+                App.getInstance().start("b", "alpha", localIP, 1337);
             }
         }).start();
 
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
-
-        /*String[] a = new String[100], b = new String[100], c = new String[100];
-        String[] ca = new String[] {"http://www.learn2crack.com/wp-content/uploads/2014/03/cover_pi-160x160.png","http://www.learn2crack.com/wp-content/uploads/2014/03/cover4-160x160.png"};
-
-        for (int i = 0; i < 100; ++i) {
-            a[i] = Integer.toString(i);
-            b[i] = Integer.toString(i);
-            c[i] = ca[i%ca.length];
-        }
-
-
-        a[0] = "Back In Black";
-        b[0] = "AC/DC";
-        c[0] = "http://upload.wikimedia.org/wikipedia/ru/a/a0/BackBlackCover.jpg";
-
-        a[1] = "Dream On";
-        b[1] = "Aerosmith";
-        c[1] = "http://blogs.ajc.com/radio-tv-talk/files/2011/08/168294.jpg";
-
-        a[2] = "Cocaine";
-        b[2] = "Eric Clapton";
-        c[2] = "http://upload.wikimedia.org/wikipedia/en/e/e7/EricClapton-Slowhand.jpg";
-
-        a[3] = "Aqualung";
-        b[3] = "Jethro Tull";
-        c[3] = "http://consequenceofsound.files.wordpress.com/2008/11/aqualung.jpg";
-
-        a[4] = "Prayer Of The Refugee";
-        b[4] = "Rise Against";
-        c[4] = "http://united-metal.ru/uploads/posts/2011-01/1295527921_album-the-sufferer-the-witness.jpg";
-
-        a[5] = "Black Hole Sun";
-        b[5] = "Soundgarden";
-        c[5] = "http://eil.com/images/main/Soundgarden+-+Superunknown+-+Orange+Vinyl+-+DOUBLE+LP-100590.jpg";
-
-        a[6] = "California Dreamin'";
-        b[6] = "The Papas And Mamas";
-        c[6] = "http://www.1000recordings.com/images/artist-m/mamas-papas-528-l.jpg";
-
-
-        ListAdapter adapter = new CustomList(this, a, b, c);*/
 
         final ListAdapter adapter = new CustomList(this, TrackStorage.getInstance().getTracks());
         TrackStorage.getInstance().addEventListener(new EventListener() {
@@ -161,7 +119,7 @@ public class HelloAndroidActivity extends Activity {
         }
 
         @Override
-        public View getView(int position, View view, ViewGroup parent) {
+        public View getView(final int position, View view, ViewGroup parent) {
             LayoutInflater inflater = context.getLayoutInflater();
             View rowView = inflater.inflate(R.layout.listitem, null, true);
             TextView txtTitle = (TextView) rowView.findViewById(R.id.line);
@@ -169,7 +127,7 @@ public class HelloAndroidActivity extends Activity {
             ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
             txtTitle.setText(items.get(position).getTrackName());
             txtSecondTitle.setText(items.get(position).getTrackArtist());
-            imageView.setImageBitmap(getImageBitmap("http://consequenceofsound.files.wordpress.com/2008/11/aqualung.jpg"));
+            imageView.setImageBitmap(getImageBitmap(getAlbumArtURL(items.get(position))));
             return rowView;
         }
 
@@ -183,15 +141,11 @@ public class HelloAndroidActivity extends Activity {
             items.add(t);
         }
 
-        @Override
-        public boolean hasStableIds() {
-            return true;
-        }
 
-        HashMap<String, Bitmap> map = new HashMap<String, Bitmap>();
+        HashMap<String, Bitmap> imageCache = new HashMap<>();
         private Bitmap getImageBitmap(final String url) {
-            if (map.containsKey(url)) {
-                return map.get(url);
+            if (imageCache.containsKey(url)) {
+                return imageCache.get(url);
             }
             final Bitmap[] bm = {null};
             Thread t = new Thread(new Runnable() {
@@ -217,8 +171,30 @@ public class HelloAndroidActivity extends Activity {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            map.put(url, bm[0]);
+            imageCache.put(url, bm[0]);
             return bm[0];
+        }
+
+        HashMap<Track, String> albumArtURLCache = new HashMap<>();
+        private String getAlbumArtURL(final Track track) {
+            if (albumArtURLCache.containsKey(track)) {
+                return albumArtURLCache.get(track);
+            }
+            final String[] result = {null};
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    result[0] = AlbumArtDownloader.getURL(track);
+                }
+            });
+            t.start();
+            try {
+                t.join(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            albumArtURLCache.put(track, result[0]);
+            return result[0];
         }
     }
 

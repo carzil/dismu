00ca5a025ac995af5b49ba27fa0f0e41fefcdc01
Alexander Andreev <carzil@yandex.ru>
2014-04-25 23:45:01 +0400

    added consturctors to TrackNotFoundExecption

--- client/src/main/java/com/dismu/exceptions/TrackNotFoundException.java
+++ client/src/main/java/com/dismu/exceptions/TrackNotFoundException.java
@@ -1,4 +1,8 @@
 package com.dismu.exceptions;
 
 public class TrackNotFoundException extends Exception {
+    public TrackNotFoundException() { super(); }
+    public TrackNotFoundException(String message) { super(message); }
+    public TrackNotFoundException(String message, Throwable cause) { super(message, cause); }
+    public TrackNotFoundException(Throwable cause) { super(cause); }
 }

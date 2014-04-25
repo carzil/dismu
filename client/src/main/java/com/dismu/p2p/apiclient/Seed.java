d1a5e36c2ae4a683964c2bb2d19ae82191cd1bfe
Alexander Andreev <carzil@yandex.ru>
2014-04-25 23:44:34 +0400

    seed hash and equals implementation

--- client/src/main/java/com/dismu/p2p/apiclient/Seed.java
+++ client/src/main/java/com/dismu/p2p/apiclient/Seed.java
@@ -10,9 +10,17 @@ public class Seed {
     public boolean equals(Object other) {
         if (other instanceof Seed) {
             Seed seed = (Seed)other;
-            return seed.userId.equals(userId) && seed.groupId.equals(groupId);
+            return seed.userId.equals(userId) && seed.groupId.equals(groupId) && seed.localIP.equals(localIP) && seed.remoteIP.equals(remoteIP) && seed.port == port;
         } else {
             return false;
         }
+    }
+
+    public int hashCode() {
+        return userId.hashCode() ^ groupId.hashCode() ^ localIP.hashCode() ^ remoteIP.hashCode() ^ port;
+    }
+
+    public String toString() {
+        return String.format("Seed[localIP=%s, remoteIP=%s, userID=%s, groupID=%s, port=%d, hashCode=%s]", localIP, remoteIP, userId, groupId, port, hashCode());
     }
 }
